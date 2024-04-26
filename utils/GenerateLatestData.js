a6980d81cb121d46d8b4befd059e086d91b4e114
juberti <justin@fixie.ai>
2024-04-26 16:59:07 -0700

    v1

--- utils/GenerateLatestData.js
+++ utils/GenerateLatestData.js
@@ -1,44 +1,31 @@
 // Define the regions to search for files
-const regions = ['cdg', 'sea', 'iad'];
+const regions = ["cdg", "sea", "iad"];
 
 // Function to fetch data from a given URL
 async function fetchData(url) {
-    try {
-        const response = await fetch(url);
-        if (!response.ok) {
-            throw new Error('File not found');
-        }
-        return await response.json();
-    } catch (error) {
-        console.error(`Error fetching data from ${url}: ${error}`);
-        return null;
-    }
+  const response = await fetch(url);
+  return await response.json();
 }
 
 // Main function to fetch files from all regions and merge them
 async function fetchAndMergeFiles(date) {
-    if (!date) {
-        console.error('Invalid date provided.');
-        return;
-    }
-    
-    const baseUrl = 'https://storage.googleapis.com/thefastest-data';
-    const promises = regions.map(region => fetchData(`${baseUrl}/${region}/text/${date}.json`));
-    const results = await Promise.all(promises);
+  if (!date) {
+    throw new Error("Invalid date provided.");
+  }
 
-    // Filter out any null results (files not found)
-    const validResults = results.filter(result => result !== null);
-
-    // If no files were found, do not update "latest.json"
-    if (validResults.length === 0) {
-        console.log('No files found for the given date. "latest.json" will not be updated.');
-        return;
-    }
-
-    // Output the merged data to the console
-    process.stdout.write(JSON.stringify(validResults, null, 2));
+  const baseUrl = "https://storage.googleapis.com/thefastest-data";
+  const promises = regions.map((region) =>
+    fetchData(`${baseUrl}/${region}/text/${date}.json`)
+  );
+  const results = await Promise.all(promises);
+  process.stdout.write(JSON.stringify(results, null, 2));
 }
 
 // Example usage with the current date
-const currentDate = new Date().toISOString().split('T')[0]; // Format: YYYY-MM-DD
-fetchAndMergeFiles(currentDate);
+const currentDate = new Date().toISOString().split("T")[0]; // Format: YYYY-MM-DD
+try {
+  fetchAndMergeFiles(currentDate);
+} catch (error) {
+  console.error(error);
+  process.exit(1);
+}
